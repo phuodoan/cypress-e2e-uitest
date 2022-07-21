5f7333fa91118e7692f8b89ea305cc8589fa0c04
phuodoan <109708481+phuodoan@users.noreply.github.com>
2022-07-21 19:14:03 +0700

    fix issue

--- get_spec.js
+++ get_spec.js
@@ -1,13 +1,14 @@
 const glob = require('glob')
 const fs = require('fs')
 
-const getSpecPath = (configPath = 'cypress.config.js') => {
-  const rawData = fs.readFileSync(configPath)
-  const config = JSON.parse(rawData)
-  const specPath = 'cypress/e2e'
-//     config.integrationFolder ?? config.e2e?.specPattern ?? 'cypress/e2e'
-  return specPath
-}
-
-const files = glob.sync(`${getSpecPath()}/*.js`)
+// const getSpecPath = (configPath = 'cypress.config.js') => {
+//   const rawData = fs.readFileSync(configPath)
+//   const config = JSON.parse(rawData)
+//   const specPath = 'cypress/e2e'
+// //     config.integrationFolder ?? config.e2e?.specPattern ?? 'cypress/e2e'
+//   return specPath
+// }
+const specPath ='cypress/e2e'
+const files = glob.sync(`${specPath}/*.js`)
+// const files = glob.sync(`${getSpecPath()}/*.js`)
 process.stdout.write(JSON.stringify(files))
