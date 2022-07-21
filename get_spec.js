c891002bd4167268677170d7b571ff471fa1c79b
phuodoan <109708481+phuodoan@users.noreply.github.com>
2022-07-21 19:07:47 +0700

    fix issue

--- get_spec.js
+++ get_spec.js
@@ -6,9 +6,8 @@ const getSpecPath = (configPath = 'cypress.config.js') => {
   const config = JSON.parse(rawData)
   const specPath =
     config.integrationFolder ?? config.e2e?.specPattern ?? 'cypress/e2e'
-  console.log(specPath)
   return specPath
 }
 
-// const files = glob.sync(`${getSpecPath()}/*.js`)
-// process.stdout.write(JSON.stringify(files))
+const files = glob.sync(`${getSpecPath()}/*.js`)
+process.stdout.write(JSON.stringify(files))
